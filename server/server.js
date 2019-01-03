a385fea391e6aa091024cf5247b799c4af049a7c
Alezco05 <jhon.mario.05hotmail.com>
2019-01-03 10:00:16 -0500

    Agregado modelo de Usuario, Agrega en la base de Datos

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 require('./config/config');
+const mongoose = require('mongoose');
 const express = require ('express');
-
 const app = express();
+
 
 const bodyParser = require('body-parser');
 //Tomar datos de la peticion
@@ -9,36 +10,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(require('./routes/usuario'));
 
 app.get('/', (req,res)=>{
     res.json('Hola mundo');
 });
 
 
-app.get('/usuario', (req,res)=>{
-    res.json(200, 'get Usario');
-});
 
-app.post('/usuario', (req,res)=>{
-    let body = req.body;
-    if(body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        });
-    }
-    res.json({
-        persona : body
-    });
-});
 
-app.put('/usuario/:id', (req,res)=>{
-    let id = req.params.id;
-    res.json({id});
-});
-
-app.delete('/usuario', (req,res)=>{
-    res.json('Delete Usario');
+mongoose.connect('mongodb://localhost:27017/cafe',(err,res) =>{
+    if(err) throw err;
+    console.log("Conexion establecida");
 });
 
 app.listen(process.env.PORT, () =>{
