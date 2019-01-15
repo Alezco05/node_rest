e5e3eb289effc9c5d5aa16b3adc5f2c9f28585b6
Alezco05 <jhon.mario.05hotmail.com>
2019-01-15 16:03:51 -0500

    Manejo de las imagenes

--- server/middlewares/autenficacion.js
+++ server/middlewares/autenficacion.js
@@ -38,8 +38,27 @@ let verificarUsuario = (req, res, next) => {
         });
     }
 };
+// ==================
+// VERIFICAR token para la imagen
+// ==================
+let verificarTokenImg = (req,res,next)=>{
+    let token = req.query.token;
+    jwt.verify(token,process.env.SEED,(err,decoded)=>{
+        if(err){
+            return res.status(401).json({
+                ok:false,
+                err:{
+                    message: "Token no valido"
+                }
+            });
+        }
+        req.usuario = decoded.usuario;
+        next();
+    });
+}
 
 module.exports = {
     verificarToken,
-    verificarUsuario
+    verificarUsuario,
+    verificarTokenImg
 }
--- server/routes/upload.js
+++ server/routes/upload.js
@@ -4,6 +4,7 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 const Usuario = require('../models/usuario');
+const Producto = require('../models/producto');
 
 
 // default options
@@ -12,7 +13,7 @@ app.use(fileUpload());
 app.put('/upload/:tipo/:id', function (req, res) {
     let tipo = req.params.tipo;
     let id = req.params.id;
-    
+
     if (Object.keys(req.files).length == 0) {
         return res.status(400).json({
             ok: false,
@@ -23,129 +24,103 @@ app.put('/upload/:tipo/:id', function (req, res) {
     }
 
     //Valida tipo 
-    const tiposValidos = ['productos','usuarios'];
-    if(tiposValidos.indexOf(tipo)<0){
+    const tiposValidos = ['productos', 'usuarios'];
+    if (tiposValidos.indexOf(tipo) < 0) {
         return res.status(400).json({
             ok: false,
             err: {
                 message: 'Los tipos validos son ' + tiposValidos.join(' y '),
-                tipo : tipo
+                tipo: tipo
             }
         });
     }
     // The name of the input field (i.e. "archivo") is used to retrieve the uploaded file
     let archivo = req.files.archivo;
     let nombreCortado = archivo.name.split('.');
-    let extension = nombreCortado[nombreCortado.length-1];
+    let extension = nombreCortado[nombreCortado.length - 1];
     // Extensiones permitidas 
-    const extensionesValida = ['png','jpg','gif','jpeg'];
-    if(extensionesValida.indexOf(extension) < 0){
+    const extensionesValida = ['png', 'jpg', 'gif', 'jpeg'];
+    if (extensionesValida.indexOf(extension) < 0) {
         return res.status(400).json({
             ok: false,
             err: {
                 message: 'La extensiones permitidas son ' + extensionesValida.join(', '),
-                ext : extension
+                ext: extension
             }
         });
     }
-
-
     //Cambiar nombre Archivo
     let nombreArchivo = `${id}-${new Date().getMilliseconds()}.${extension}`;
 
-    archivo.mv(`uploads/${ tipo }/${nombreArchivo}`, (err) => {
+    archivo.mv(`uploads/${tipo}/${nombreArchivo}`, (err) => {
         if (err) {
-            return res.status(500).json({ok: false,err});
+            return res.status(500).json({ ok: false, err });
         }
-
-        // No enviamos el objeto Response
-        imagenUsuario(id, nombreArchivo, (user) =>{
-            if(user.ok){
-               const {status, ok, message} = user //Destructuring ES6
-               return res.status(status).json({ok:ok,message:message});//Tambien ...json({ok, message}) ES6
-            }else{
-               const {status, ok, err} = user //Destructuring ES6
-               return res.status(status).json({ok:ok,err:err}); //Tambien ...json({ok, err}) ES6 
-            }
-        });  
+        if (tipo === 'usuarios') {
+            // No enviamos el objeto Response
+            imagenUsuario(id, nombreArchivo, (user) => {
+                const { status, ok, message } = user //Destructuring ES6
+                if (user.ok) {
+                    return res.status(status).json({ ok: ok, message: message, user: user });//Tambien ...json({ok, message}) ES6
+                } else {
+                    return res.status(status).json({ ok: ok, err: err }); //Tambien ...json({ok, err}) ES6 
+                }
+            });
+        }
+        else if (tipo === 'productos') {
+            imagenProducto(id, nombreArchivo, (producto) => {
+                const { status, ok, message } = producto //Destructuring ES6
+                if (producto.ok) {
+                    return res.status(status).json({ ok: ok, message: message, producto: producto });//Tambien ...json({ok, message}) ES6
+                } else {
+                    return res.status(status).json({ ok: ok, err: err }); //Tambien ...json({ok, err}) ES6 
+                }
+            });
+        }
     });
-
-    // ANTIGUA
-    /* archivo.mv(`uploads/${ tipo }/${nombreArchivo}`, (err) => {
-        if (err) {
-            return res.status(500).json({
-                ok: false,
-                err
-            });
-        }
-        imagenUsuario(id,res,nombreArchivo);
-        //Imagen Cargada
-
-        res.json({
-            ok: true,
-            message: 'Archivo enviado'
-        })
-    }); */
 });
-
-/* function imagenUsuario(id,res,nombreArchivo){
-    Usuario.findById(id,(err,usuarioDB)=>{
+function imagenUsuario(id, nombreArchivo, callback) {
+    Usuario.findById(id, (err, usuarioDB) => {
         if (err) {
-            return res.status(500).json({
-                ok: false,
-                err
-            });
+            callback({ status: 500, ok: false, err: err.message })
+            borrarArchivo(nombreArchivo, 'usuarios');
         }
-        if(!usuarioDB){
-            return res.status(400).json({
-                ok: false,
-                err: {message: 'No existe el usuario'
-            }});
+        if (!usuarioDB) {
+            callback({ status: 400, ok: false, err: 'No existe el usuario' })
+            borrarArchivo(nombreArchivo, 'usuarios');
         }
-        let pathImage = path.resolve(__dirname,`../../uploads/usuarios/${usuarioDB}`);
-        if(fs.existsSync(pathImage)){
-            fs.unlinkSync(pathImage);
-        }
-        usuarioDB.img = nombreArchivo;
-        usuarioDB.save((err,usuarioGuardado)=>{
-            if (err) {
-                return res.status(500).json({
-                    ok: false,
-                    err
-                });
-            }
-            res.json({
-                ok: true,
-                usuario: usuarioGuardado
-            })
-        })
-    })
-} */
-
-function imagenUsuario(id, nombreArchivo, callback){
-    Usuario.findById(id, (err, usuarioDB) =>{
-        if (err) 
-            callback({status:500, ok:false, err:err.message})
-
-        if(!usuarioDB)
-            callback({status:400, ok:false, err:'No existe el usuario'})
-
-        let pathImage = path.resolve(__dirname,`../../uploads/usuarios/${usuarioDB.img}`);
-        if(fs.existsSync(pathImage)){
-            fs.unlinkSync(pathImage);
-        }
+        borrarArchivo(usuarioDB.img, 'usuarios');
         usuarioDB.img = nombreArchivo;
         usuarioDB.save((err, usuarioGuardado) => {
-            if (err) 
-                 callback({status:500, ok:false, err:err.message})
-
-            callback({status:200, ok:true, message: 'Archivo guardado'})
+            if (err) { callback({ status: 500, ok: false, err: err.message }) }
+            callback({ status: 200, ok: true, message: 'Archivo guardado', usuario: usuarioGuardado })
         })
     })
 }
 
-function imagenProducto(){
-    
+function imagenProducto(id, nombreArchivo, callback) {
+    Producto.findById(id, (err, productoDB) => {
+        if (err) {
+            callback({ status: 500, ok: false, err: err.message })
+            borrarArchivo(nombreArchivo, 'productos');
+        }
+        if (!productoDB) {
+            callback({ status: 400, ok: false, err: 'No existe el producto' })
+            borrarArchivo(nombreArchivo, 'productos');
+        }
+        borrarArchivo(productoDB.img, 'productos');
+        productoDB.img = nombreArchivo;
+        productoDB.save((err, productoGuardado) => {
+            if (err) { callback({ status: 500, ok: false, err: err.message }) }
+            callback({ status: 200, ok: true, message: 'Archivo guardado', Producto: productoGuardado })
+        })
+    })
 }
 
+function borrarArchivo(nombreImagen, tipo) {
+    let pathImage = path.resolve(__dirname, `../../uploads/${tipo}/${nombreImagen}`);
+    if (fs.existsSync(pathImage)) {
+        fs.unlinkSync(pathImage);
+    }
+}
 module.exports = app;
